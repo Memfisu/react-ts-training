b664e3dcf6a338867e58b66952fa189b4ace1538
Memfisu <memfiswho@gmail.com>
2022-01-18 18:25:45 +0400

    junior level complete

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -2,23 +2,36 @@ import React, { ChangeEvent, FC, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setData } from '../reducers/dataSetter';
 
+const appropriateLength = 10; // Новая запись добавляется при условии, что её длина меньше N символов или равна ей;
+
 const InputComponent: FC = () => {
   const dispatch = useDispatch();
   const [value, setValue] = useState<string>('');
+  const [error, setError] = useState<boolean>(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
       setValue(e.target.value);
+      (e.target.value?.length > appropriateLength) ? setError(true) :setError(false);
   };
 
   const handleClick = (): void => {
-      dispatch(setData({ data: value }));
-      setValue('');
+      if (!error) {
+          dispatch(setData({ data: value }));
+          setValue('');
+      }
   };
 
   return (
-    <div className="inputContainer">
-      <input onChange={handleChange} value={value} />
-      <button className="commonButton" onClick={handleClick}>+</button>
+    <div className="inputComponent">
+        <div className="inputContainer">
+          <input onChange={handleChange} value={value} />
+            {error &&
+                <div className="errorBox">
+                    {`Длина не должна превышать ${appropriateLength} символов`}
+                </div>
+            }
+        </div>
+      <button className="commonButton" onClick={handleClick} disabled={error}>+</button>
     </div>
   );
 }
